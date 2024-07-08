2cd3482197fbd745b999883592c7dba24b9ce7a8
minikraken <maxime.huylebroeck@outlook.com>
2024-07-08 23:27:01 +0200

    Fix: manifest update So we have better mobile colors in dead spaces

--- src/lib/components/CookieBanner.svelte
+++ src/lib/components/CookieBanner.svelte
@@ -7,8 +7,29 @@
 	let show = $state<boolean>(false)
 	let closeAnimation = $state<boolean>(false)
 
+	const setCookie = (name: string, value: string = '', days?: number) => {
+		let expires = ''
+		if (days) {
+			let date = new Date()
+			date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
+			expires = '; expires=' + date.toUTCString()
+		}
+		document.cookie = name + '=' + value + expires + '; path=/'
+	}
+
+	const getCookie = (name: string) => {
+		let nameEQ = name + '='
+		let ca = document.cookie.split(';')
+		for (let i = 0; i < ca.length; i++) {
+			let c = ca[i]
+			while (c.charAt(0) == ' ') c = c.substring(1, c.length)
+			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
+		}
+		return null
+	}
+
 	const accept = () => {
-		localStorage.setItem('cookie-accepted', 'true')
+		setCookie('cookie-accepted', 'true', 365)
 		closeAnimation = true
 		setTimeout(() => {
 			show = false
@@ -16,7 +37,7 @@
 	}
 
 	onMount(() => {
-		show = !localStorage.getItem('cookie-accepted')
+		show = !getCookie('cookie-accepted')
 	})
 </script>
 
