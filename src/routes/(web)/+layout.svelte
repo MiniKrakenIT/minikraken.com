f90e18989a5e37d1afad48552764e39120312906
minikraken <maxime.huylebroeck@outlook.com>
2025-02-10 16:46:01 +0100

    Initial commit

--- src/routes/(web)/+layout.svelte
+++ src/routes/(web)/+layout.svelte
@@ -6,13 +6,13 @@ let { children } = $props()
 </script>
 
 <svelte:head>
-	<link rel="preload" as="font" href="/fonts/Axi-Regular.woff2" type="font/woff2" crossorigin="anonymous">
+	<!--<link rel="preload" as="font" href="/fonts/Axi-Regular.woff2" type="font/woff2" crossorigin="anonymous">-->
 	<link rel="preload" as="font" href="/fonts/Axi-Bold.woff2" type="font/woff2" crossorigin="anonymous">
-	<link rel="preload" as="font" href="/fonts/Axi-Black.woff2" type="font/woff2" crossorigin="anonymous">
-	<link rel="preload" as="font" href="/fonts/Axi-Light.woff2" type="font/woff2" crossorigin="anonymous">
-	<link rel="preload" as="font" href="/fonts/Axi-Medium.woff2" type="font/woff2" crossorigin="anonymous">
-	<link rel="preload" as="font" href="/fonts/Axi-SemiBold.woff2" type="font/woff2" crossorigin="anonymous">
-	<link rel="preload" as="font" href="/fonts/Axi-Thin.woff2" type="font/woff2" crossorigin="anonymous">
+	<!--<link rel="preload" as="font" href="/fonts/Axi-Black.woff2" type="font/woff2" crossorigin="anonymous">
+	<link rel="preload" as="font" href="/fonts/Axi-Light.woff2" type="font/woff2" crossorigin="anonymous">-->
+	<link rel="preload" as="font" href="/fonts/Axi-Medium.woff2" type="font/woff2" crossorigin="anonymous">-->
+	<!--<link rel="preload" as="font" href="/fonts/Axi-SemiBold.woff2" type="font/woff2" crossorigin="anonymous">
+	<link rel="preload" as="font" href="/fonts/Axi-Thin.woff2" type="font/woff2" crossorigin="anonymous">-->
 </svelte:head>
 
 <div class="contents">
