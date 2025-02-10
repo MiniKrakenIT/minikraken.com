324a6cef6e2e96391df0d36c95fc9eb6f2a24af0
minikraken <maxime.huylebroeck@outlook.com>
2025-02-10 16:30:47 +0100

    Initial commit

--- src/lib/sections/home/Navigation.svelte
+++ src/lib/sections/home/Navigation.svelte
@@ -1,10 +1,27 @@
 <script lang="ts">
+import Menu from '$assets/icons/Menu.svelte'
+import X from '$assets/icons/X.svelte'
 import logo from '$assets/logo/minikraken-small.svg'
+
+let openMobileMenu = $state(false)
+
+const toggleMobileMenu = () => {
+	openMobileMenu = !openMobileMenu
+}
 </script>
 
-<header class="fixed z-100 w-full mt-7 sm:mt-10 md:mt-20">
+{#snippet navigation()}
+	<nav class="relative md:flex">
+			<a class="nav-items" href="/pricing">Pricing</a>
+			<a class="nav-items" href="/company">Company</a>
+			<a class="nav-items" href="/blog">Blog</a>
+			<a class="nav-items" href="/login">Login</a>
+	</nav>
+{/snippet}
+
+<header class="fixed z-100 w-full mt-8 sm:mt-12 lg:mt-20">
 	<div class="w-full">
-		<div class="relative flex justify-between w-9/10 mx-auto">
+		<div class="relative flex justify-between items-center w-full px-8 sm:px-12 lg:px-20 border-box mx-auto">
 			<div class="relative flex items-center justify-center">
 				<a class="w-14" title="Home" href="/" aria-label="Home" >
 					<img
@@ -14,45 +31,32 @@ import logo from '$assets/logo/minikraken-small.svg'
 						loading="eager"
 					/>
 				</a>
-				<div class="text-neutral font-bold sm:text-2xl md:text-4xl hidden sm:block">MiniKraken</div>
+				<div class="text-gray-950 font-bold text-3xl lg:text-4xl hidden sm:block">MiniKraken</div>
 			</div>
-			<nav class="relative hidden lg:flex">
-				<div class="relative flex group/item relative">
-					<a class="flex items-center px-4 py-3 text-base font-medium text-gray-950 bg-blend-multiply data-hover:bg-black/[2.5%]" data-headlessui-state="" href="/pricing">Pricing</a>
-				</div>
-				<div class="relative flex group/item relative">
-					<a class="flex items-center px-4 py-3 text-base font-medium text-gray-950 bg-blend-multiply data-hover:bg-black/[2.5%]" data-headlessui-state="" href="/company">Company</a>
-				</div>
-				<div class="relative flex group/item relative">
-					<a class="flex items-center px-4 py-3 text-base font-medium text-gray-950 bg-blend-multiply data-hover:bg-black/[2.5%]" data-headlessui-state="" href="/blog">Blog</a>
-				</div>
-				<div class="relative flex group/item relative">
-					<a class="flex items-center px-4 py-3 text-base font-medium text-gray-950 bg-blend-multiply data-hover:bg-black/[2.5%]" data-headlessui-state="" href="/login">Login</a>
-				</div>
-			</nav>
-		</div>
-	</div>
-
-	<div class="lg:hidden" id="headlessui-disclosure-panel-:rd:" data-headlessui-state="open" data-open="">
-		<div class="flex flex-col gap-6 py-4">
-			<div style="opacity: 1; will-change: auto; transform: none;">
-				<a class="text-base font-medium text-gray-950" data-headlessui-state="" href="/pricing">Pricing</a>
+			<div class="hidden md:block">
+				{@render navigation()}
 			</div>
-			<div style="opacity: 1; will-change: auto; transform: none;">
-				<a class="text-base font-medium text-gray-950" data-headlessui-state="" href="/company">Company</a>
-			</div>
-			<div style="opacity: 1; will-change: auto; transform: none;">
-				<a class="text-base font-medium text-gray-950" data-headlessui-state="" href="/blog">Blog</a>
-			</div>
-			<div style="opacity: 1; will-change: auto; transform: none;"><a class="text-base font-medium text-gray-950" data-headlessui-state="" href="/login">Login</a>
-			</div>
-		</div>
-		<div class="absolute left-1/2 w-screen -translate-x-1/2">
-			<div class="absolute inset-x-0 top-0 border-t border-black/5"></div>
-			<div class="absolute inset-x-0 top-2 border-t border-black/5"></div>
+			<Menu class="md:hidden p-2 color-slate-950 select-none w-13 h-13" stroke-width="1.8" onclick={toggleMobileMenu}></Menu>
 		</div>
 	</div>
 </header>
 
-<style>
+{#if openMobileMenu}
+	<div class="absolute z-200 top-0 left-0 w-full h-full flex justify-center items-center">
+		<div class="absolute w-full h-full backdrop-blur-md background"></div>
+		<X class="color-base-content w-16 h-16 absolute top-7 right-6 sm:top-11 sm:right-10 p-2 select-none" stroke-width="1.5" onclick={toggleMobileMenu}/>
+		<div class="flex relative w-8/10">
+			{@render navigation()}
+		</div>
+	</div>
+{/if}
+
+<style lang="postcss">
+	.background {
+		background-image: radial-gradient(rgba(0,0,0,.8) 2px, black 2px);
+		background-size: 10px 10px;
+	}
+	.nav-items {
+			@apply: flex items-center p-4 font-bold md:font-medium hover:underline text-[10vw] md:text-4 md:text-base-300 bg-blend-multiply;
+	}
 </style>
--- src/routes/(web)/+page.svelte
+++ src/routes/(web)/+page.svelte
@@ -3,14 +3,14 @@
 
 <div class="overflow-hidden">
 	<div class="relative">
-		<div class="absolute inset-2 bottom-0 rounded-8 ring-1 ring-black/5 ring-inset bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#fff1be] from-28% via-[#ee87cb] via-70% to-[#b060ff] sm:bg-[linear-gradient(145deg,var(--tw-gradient-stops))]"></div>
+		<div class="absolute inset-3 bottom-0 rounded-8 ring-1 ring-black/5 ring-inset bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#fff1be] from-28% via-[#ee87cb] via-70% to-[#b060ff] sm:bg-[linear-gradient(145deg,var(--tw-gradient-stops))]"></div>
 		<div class="relative px-6 lg:px-8">
 			<div class="mx-auto max-w-2xl lg:max-w-7xl">
 				<div class="pt-26 pb-14 sm:pt-30 sm:pb-22 md:pt-55 md:pb-48">
-					<h1 class="font-display text-6xl/[0.9] font-medium tracking-tight text-balance text-base-300 sm:text-8xl/[0.8] md:text-9xl/[0.8]">
+					<h1 class="font-display text-6xl/[0.9] font-medium tracking-tight text-balance text-gray-950 sm:text-8xl/[0.8] md:text-9xl/[0.8]">
 						Close every deal.
 					</h1>
-					<p class="mt-8 max-w-lg text-xl/7 font-medium text-gray-950/75 sm:text-2xl/8">
+					<p class="mt-8 max-w-lg text-xl/7 font-medium text-gray-950 sm:text-2xl/8">
 						Radiant helps you sell more by revealing sensitive information about your customers.
 					</p>
 					<div class="mt-12 flex flex-col gap-x-6 gap-y-4 sm:flex-row">
