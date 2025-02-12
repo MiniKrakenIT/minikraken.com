ab464164df4c01ba0c0d9683387f1f758fe66dbc
minikraken <maxime.huylebroeck@outlook.com>
2025-02-12 22:42:29 +0100

    Initial commit

--- src/lib/sections/navigation/Navigation.svelte
+++ src/lib/sections/navigation/Navigation.svelte
@@ -5,17 +5,7 @@ import X from '$assets/icons/X.svelte'
 import logo from '$assets/logo/minikraken-small.svg'
 import Link from '$components/base/Link/Link.svelte'
 import { Behavior } from '$components/props'
-
-let openMobileMenu = $state(false)
-
-const toggleMobileMenu = () => {
-	openMobileMenu = !openMobileMenu
-	if (browser && openMobileMenu) {
-		document.body.style.overflow = 'hidden'
-	} else {
-		document.body.style.overflow = 'auto'
-	}
-}
+import { onMount } from 'svelte'
 
 const navigations = [
 	{
@@ -35,18 +25,41 @@ const navigations = [
 		href: '/login'
 	}
 ]
+
+let openMobileMenu = $state(false)
+let showMenuBackground = $state(false)
+
+const toggleMobileMenu = () => {
+	openMobileMenu = !openMobileMenu
+	if (browser && openMobileMenu) {
+		document.body.style.overflow = 'hidden'
+	} else {
+		document.body.style.overflow = 'auto'
+	}
+}
+
+onMount(() => {
+	const onScroll = () => (showMenuBackground = window.scrollY >= 40)
+
+	window.addEventListener('scroll', onScroll, { passive: true })
+
+	return () => {
+		window.removeEventListener('scroll', onScroll)
+	}
+})
 </script>
 
 {#snippet navigation()}
 	<nav class="relative md:flex">
 		{#each navigations as {name, href}}
-			<Link class="flex items-center p-4 font-bold md:font-medium text-[10vw] md:text-4 md:text-base-300 bg-blend-multiply" behavior={Behavior.hover} {href}>{name}</Link>
+			<Link class="flex items-center p-4 font-bold md:font-medium text-[10vw] md:text-4 text-slate-950 bg-blend-multiply" behavior={Behavior.hover} {href}>{name}</Link>
 		{/each}
 	</nav>
 {/snippet}
 
 <header class="fixed w-full z-100 mt-8 sm:mt-12 lg:mt-20">
-	<div class="relative box-border flex justify-between items-center px-8 sm:px-12 lg:px-20 box-border mx-auto">
+	<span class="absolute inset-3 -top-1/4 backdrop-blur-md rounded-8 h-20 mx-3 bg-light-700/50 shadow-xl opacity-0 transition-all transition-duration-400" class:opacity-100={showMenuBackground}></span>
+	<div class="relative box-border flex justify-between items-center mx-8 sm:mx-12 lg:mx-20">
 		<a class="w-14 flex items-center" title="Home" href="/" aria-label="Home" >
 			<img
 				class="object-cover object-center"
@@ -54,7 +67,7 @@ const navigations = [
 				alt="MiniKraken Logo"
 				loading="eager"
 			/>
-			<div class="text-gray-950 font-bold text-3xl lg:text-4xl hidden sm:block">MiniKraken</div>
+			<div class="color-slate-950 font-bold text-3xl lg:text-4xl hidden sm:block">MiniKraken</div>
 		</a>
 		<div class="hidden md:block">
 			{@render navigation()}
@@ -65,7 +78,7 @@ const navigations = [
 
 {#if openMobileMenu}
 	<div class="fixed z-200 inset-0 w-full h-full flex justify-center items-center">
-		<div class="absolute w-full h-full backdrop-blur-8 menu-background"></div>
+		<div class="absolute w-full h-full backdrop-blur-8 mobile-menu-background"></div>
 		<X class="color-base-content w-16 h-16 absolute z-205 top-7 right-6 sm:(top-11 right-10) p-2 select-none" stroke-width="1.5" onclick={toggleMobileMenu}/>
 		<div class="flex relative w-80%">
 			{@render navigation()}
@@ -74,7 +87,7 @@ const navigations = [
 {/if}
 
 <style>
-	.menu-background {
+	.mobile-menu-background {
 		background-image: radial-gradient(color-mix(in oklch, var(--color-base-100) 65%, transparent 35%) 2px, var(--color-base-100) 2px);
 		background-size: 10px 10px;
 	}
