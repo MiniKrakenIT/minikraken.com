fdc5c9c6995adea8a96f93080f355f73c3e3e945
minikraken <maxime.huylebroeck@outlook.com>
2025-02-12 23:13:02 +0100

    Initial commit

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,12 +1,13 @@
 import type { ClientInit, HandleClientError } from '@sveltejs/kit'
-import posthog from 'posthog-js'
+/*import posthog from 'posthog-js'
 
 posthog.init('phc_UR8XJeOwzfProVxSc3L9xf9gfYRjzQdBHP2GG1ccLsD', {
 	api_host: 'https://eu.i.posthog.com',
-	person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+	person_profiles: 'always', // or 'always' to create profiles for anonymous users as well,
+	disable_session_recording: true
 })
 
-posthog.capture('page-loaded', { property: 'value' })
+posthog.capture('page-loaded', { property: 'value' })*/
 
 export const init: ClientInit = () => {}
 export const handleError: HandleClientError = async ({ message }) => {
@@ -14,6 +15,6 @@ export const handleError: HandleClientError = async ({ message }) => {
 
 	return {
 		message: message ?? 'Whoops!',
-		errorId: crypto.randomUUID()
+		errorId: errorId
 	}
 }
