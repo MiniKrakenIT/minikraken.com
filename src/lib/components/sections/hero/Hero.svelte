45f996dcc0492babb5f8a490c9a712c9ec589290
minikraken <maxime.huylebroeck@outlook.com>
2025-02-24 23:28:22 +0100

    Initial commit

---
+++ src/lib/components/sections/hero/Hero.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+import Button from '$components/base/Button/Button.svelte'
+import { Color, Size, Variant } from '$components/props'
+</script>
+
+<div class="overflow-hidden bg-base-300">
+	<div class="relative">
+		<div class="absolute inset-2 sm:inset-3 rounded-8 ring-1 ring-black/5 ring-inset bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#fff1be] from-28% via-[#ee87cb] via-70% to-[#b060ff] sm:bg-[linear-gradient(145deg,var(--tw-gradient-stops))]"></div>
+		<div class="relative px-6 lg:px-8">
+			<div class="mx-auto max-w-2xl lg:max-w-7xl">
+				<div class="pt-26 pb-14 sm:pt-30 sm:pb-22 md:pt-45 md:pb-48">
+					<h1 class="mt-4 md:mt-0 text-6xl font-medium tracking-tight text-balance text-gray-950 sm:text-8xl md:text-9xl">
+						Close every deal.
+					</h1>
+					<p class="mt-6 max-w-lg text-xl font-medium text-gray-950 sm:text-2xl">
+						Radiant helps you sell more by revealing sensitive information about your customers.
+					</p>
+					<div class="flex flex-col sm:flex-row gap-x-6 gap-y-4 mt-12  sm:justify-center md:justify-start">
+						<Button variant={Variant.soft} size={{base: Size.md, md: Size.lg}}>
+							Get started
+						</Button>
+						<Button variant={Variant.outline} color={Color.neutral} size={{base: Size.md, md: Size.lg}}>
+							See pricing
+						</Button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+
+</style>
--- src/routes/(web)/+page.svelte
+++ src/routes/(web)/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-import Button from '$components/base/Button/Button.svelte'
-import { Color, Size, Variant } from '$components/props'
-import CtaSection from '$lib/sections/cta/CtaSection.svelte'
-import BentoGrid from '$lib/sections/grid/BentoGrid.svelte'
+import CtaSection from '$lib/components/sections/cta/CtaSection.svelte'
+import BentoGrid from '$lib/components/sections/grid/BentoGrid.svelte'
+import Hero from '$lib/components/sections/hero/Hero.svelte'
+import { store } from '$stores/navigation'
+
+store.set({ fixedBackground: false })
 
 const heroStrings = [
 	'Radiant helps you sell more by revealing sensitive information about your customers.',
@@ -16,32 +18,6 @@ const heroStrings = [
 	<meta name="description" content="Wij helpen jou als KMO efficienter te werken door het gebruik van software dat werkt."/>
 </svelte:head>
 
-<div class="overflow-hidden bg-base-300">
-	<div class="relative">
-		<div class="absolute inset-2 sm:inset-3 rounded-8 ring-1 ring-black/5 ring-inset bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#fff1be] from-28% via-[#ee87cb] via-70% to-[#b060ff] sm:bg-[linear-gradient(145deg,var(--tw-gradient-stops))]"></div>
-		<div class="relative px-6 lg:px-8">
-			<div class="mx-auto max-w-2xl lg:max-w-7xl">
-				<div class="pt-26 pb-14 sm:pt-30 sm:pb-22 md:pt-45 md:pb-48">
-					<h1 class="mt-4 md:mt-0 text-6xl font-medium tracking-tight text-balance text-gray-950 sm:text-8xl md:text-9xl">
-						Close every deal.
-					</h1>
-					<p class="mt-6 max-w-lg text-xl font-medium text-gray-950 sm:text-2xl">
-						Radiant helps you sell more by revealing sensitive information about your customers.
-					</p>
-					<div class="flex flex-col sm:flex-row gap-x-6 gap-y-4 mt-12  sm:justify-center md:justify-start">
-						<Button variant={Variant.soft} size={{base: Size.md, md: Size.lg}}>
-							Get started
-						</Button>
-						<Button variant={Variant.outline} color={Color.neutral} size={{base: Size.md, md: Size.lg}}>
-							See pricing
-						</Button>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-
+<Hero/>
 <BentoGrid/>
-
 <CtaSection/>
