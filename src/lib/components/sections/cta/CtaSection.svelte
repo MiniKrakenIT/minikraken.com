1de3d88ae911a44e33864b77f0964dbedac531ab
minikraken <maxime.huylebroeck@outlook.com>
2025-02-26 23:06:38 +0100

    Initial commit

--- src/lib/components/sections/cta/CtaSection.svelte
+++ src/lib/components/sections/cta/CtaSection.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 import Button from '$components/base/Button/Button.svelte'
-import { Variant } from '$components/props.js'
+import { Variant } from '$components/props'
+import { inViewIntro } from '$lib/utils/motion/in-view-intro'
 </script>
 
 <div class="relative rounded-8 bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#fff1be] from-28% via-[#ee87cb] via-70% to-[#b060ff]">
@@ -9,11 +10,11 @@ import { Variant } from '$components/props.js'
 		<div class="mx-auto max-w-2xl lg:max-w-7xl">
 			<div class="relative pt-20 pb-16 text-center sm:py-24">
 				<hgroup>
-					<h2 class="text-xs/5 font-semibold tracking-widest uppercase text-passive-100">Get started</h2>
-					<p class="mt-6 text-3xl font-medium tracking-tight sm:text-5xl">Ready to dive in?<br>Start your free trial today.</p>
+					<h2 use:inViewIntro={2} class="text-xs/5 font-semibold tracking-widest uppercase text-passive-100">Get started</h2>
+					<p use:inViewIntro={2} class="mt-6 text-3xl font-medium tracking-tight sm:text-5xl">Ready to dive in?<br>Start your free trial today.</p>
 				</hgroup>
-				<p class="mx-auto mt-6 max-w-xs text-sm/6 text-passive-100">Get the cheat codes for selling and unlock your team's revenue potential.</p>
-				<div class="mt-6">
+				<p use:inViewIntro={2} class="mx-auto mt-6 max-w-xs text-sm/6 text-passive-100">Get the cheat codes for selling and unlock your team's revenue potential.</p>
+				<div use:inViewIntro={2} class="mt-6">
 					<Button class="w-full sm:w-auto inline-flex items-center justify-center" variant={Variant.soft}>Get started</Button>
 				</div>
 			</div>
