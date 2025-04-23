f995410d998fb085448c93bf5712a37175e8acbc
minikraken <maxime.huylebroeck@outlook.com>
2025-04-23 22:54:07 +0200

    fix: fix biome issues

--- src/lib/components/Dropdown/Dropdown.svelte
+++ src/lib/components/Dropdown/Dropdown.svelte
@@ -1,3 +1,39 @@
+<script module lang="ts">
+export enum Alignment {
+	start = 'start',
+	center = 'center',
+	end = 'end'
+}
+
+export enum Placement {
+	top = 'top',
+	left = 'left',
+	right = 'right',
+	bottom = 'bottom'
+}
+
+export enum Modifier {
+	hover = 'hover',
+	open = 'open'
+}
+
+export interface ListAnchor {
+	text: string
+	href: string
+}
+
+export interface ListButton {
+	text: string
+	clickHandler: () => void
+}
+
+export type ListItem = ListAnchor | ListButton
+
+export type ListItems = ListItem[]
+
+export const isListAnchor = (item: ListItem): item is ListAnchor =>
+	(item as ListAnchor).href !== undefined
+</script>
 <script lang="ts">
 import { nanoid } from 'nanoid/non-secure'
 import type { Snippet } from 'svelte'
